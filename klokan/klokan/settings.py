90558c7ae6439074777124ddab7def36d9489ca9
utp120 <72672709+JantarPantar@users.noreply.github.com>
2025-06-26 15:38:47 +0200

    compose for production

--- klokan/klokan/settings.py
+++ klokan/klokan/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
 STATIC_ROOT = '/static/'
 
 # Quick-start development settings - unsuitable for production
@@ -131,8 +131,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
